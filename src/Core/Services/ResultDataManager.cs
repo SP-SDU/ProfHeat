b5757acd6de0f3402dfa77fe8a5a441fbb6023b9
Mathias <tisen2013@gmail.com>
2024-04-08 19:01:56 +0200

    Proper structure
    Added:
    * Properly adhering to root folder structure convention
    * Seperation of Core logic and gui
    * Proper solution setup including solution folders and solution items

---
+++ src/Core/Services/ResultDataManager.cs
@@ -0,0 +1,59 @@
+// Copyright 2024 SoftFuzz
+//
+// Licensed under the Apache License, Version 2.0 (the "License"):
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using ProfHeat.Core.Models;
+using ProfHeat.Core.Interfaces;
+using System.Globalization;
+using System.Text;
+
+namespace ProfHeat.Core.Services;
+
+public class ResultDataManager(string filePath) : IResultDataManager
+{
+    private readonly char _delimiter = CultureInfo.CurrentCulture.TextInfo.ListSeparator[0];
+
+    // Loads optimization results from a CSV file.
+    public List<OptimizationResult> LoadOptimizationResults()
+    {
+        var results = new List<OptimizationResult>();
+        foreach (var line in File.ReadAllLines(filePath).Skip(1).ToList()) // Skip the header line
+        {
+            var columns = line.Split(_delimiter);
+
+            var result = new OptimizationResult(
+                DateTime.Parse(columns[0].Trim()),
+                double.Parse(columns[1].Trim(), CultureInfo.InvariantCulture),
+                double.Parse(columns[2].Trim(), CultureInfo.InvariantCulture),
+                double.Parse(columns[3].Trim(), CultureInfo.InvariantCulture));
+
+            results.Add(result);
+        }
+
+        return results;
+    }
+
+    // Saves optimization results to a CSV file.
+    public void SaveOptimizationResults(List<OptimizationResult> results)
+    {
+        var sb = new StringBuilder();
+        _ = sb.AppendLine($"Time{_delimiter}OptimizedHeat{_delimiter}OptimizedCosts{_delimiter}CO2Emissions");
+
+        foreach (var result in results)
+        {
+            _ = sb.AppendLine($"{result.Time}{_delimiter}{result.OptimizedHeat}{_delimiter}{result.OptimizedCosts}{_delimiter}{result.CO2Emissions}");
+        }
+
+        File.WriteAllText(filePath, sb.ToString());
+    }
+}
---
+++ src/GUI/ViewLocator.cs
@@ -0,0 +1,44 @@
+// Copyright 2024 SoftFuzz
+//
+// Licensed under the Apache License, Version 2.0 (the "License"):
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using ProfHeat.GUI.ViewModels;
+using System;
+
+namespace ProfHeat.GUI;
+public class ViewLocator : IDataTemplate
+{
+    public Control? Build(object? data)
+    {
+        if (data is null)
+        {
+            return null;
+        }
+
+        var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            var control = (Control) Activator.CreateInstance(type)!;
+            control.DataContext = data;
+            return control;
+        }
+
+        return new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public bool Match(object? data) => data is ViewModelBase;
+}
