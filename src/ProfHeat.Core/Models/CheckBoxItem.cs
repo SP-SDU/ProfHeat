e96dcbeb75f958b3021cb609403608d142e74038
Mathias <tisen2013@gmail.com>
2024-04-24 16:12:40 +0200

    Update CheckBoxItem.cs

--- src/ProfHeat.Core/Models/CheckBoxItem.cs
+++ src/ProfHeat.Core/Models/CheckBoxItem.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 namespace ProfHeat.Core.Models;
 
 public partial class CheckBoxItem(string name, string imagePath, Action isCheckedChanged) : ObservableObject
